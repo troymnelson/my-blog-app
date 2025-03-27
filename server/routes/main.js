4a4713c1229f06d9769d54ae081b95f89c9808c2
Troy Nelson <Troymn625@gmail.com>
2025-03-27 13:48:41 -0500

    added locals

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,7 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 router.get('', (req, res) => {
-    res.render('index');
+    const locals = {
+        title: "Troy's Blog",
+        description: "Simple blog created with NodeJs, Express and MongoDB."
+    }
+
+    res.render('index', {locals});
+});
+
+router.get('/about', (req, res) => {
+    res.render('about');
 })
 
 module.exports = router;
