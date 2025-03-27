067535daf73fd453ae99db8dc3d9f95b7bbe9ecd
troymnelson <troymn625@gmail.com>
2025-03-27 17:09:23 -0500

    added post and style

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,14 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../models/Post');
 
-router.get('', (req, res) => {
+/***
+ * GET /
+ * HOME
+ */
+
+router.get('', async (req, res) => {
     const locals = {
         title: "Troy's Blog",
         description: "Simple blog created with NodeJs, Express and MongoDB."
     }
 
-    res.render('index', {locals});
+    try {
+        const data = await Post.find();
+        res.render('index', {locals, data});
+    } catch (err) {
+        console.log(err.message);
+    }
 });
+
+
+
+
 
 router.get('/about', (req, res) => {
     res.render('about');
