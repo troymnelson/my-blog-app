20e3d24c7b79aa6290faf8034b6f4a74ca278dd1
troymnelson <troymn625@gmail.com>
2025-03-27 13:22:24 -0500

    skeleton

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 require('dotenv').config();
 
 const express = require('express');
+const expressLayout = require('express-ejs-layouts');
 
 const app = express();
-const PORT = 5000 || process.env.PORT;
+const PORT = 3000 || process.env.PORT;
 
-app.get('', (req, res) => {
-    res.send("hello")
-})
+app.use(express.static('public'))
+
+app.use(expressLayout);
+app.set('layout', './layouts/main')
+app.set('view engine', 'ejs');
+
+app.use('/', require('./server/routes/main'));
 
 app.listen(PORT, () => {
-    console.log(`server listening on ${PORT}`)
+    console.log(`server listening on http://localhost:${PORT}`)
 })
