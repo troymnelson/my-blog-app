8af08c57811d42c2367bf3f1130bd7b5bcaa5cd3
Troy Nelson <Troymn625@gmail.com>
2025-03-27 13:40:17 -0500

    added public

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,9 +1,8 @@
 const express = require('express');
-
 const router = express.Router();
 
 router.get('', (req, res) => {
-    res.send("hello")
+    res.render('index');
 })
 
 module.exports = router;
